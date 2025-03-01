e3ab5832e25ad16b4a01db84d7e5c6e84acb5ecf
download-all <osareghea@gmail.com>
2025-03-01 20:20:06 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -20,3 +20,20 @@ function downloadMedia() {
         }
     });
 }
+function downloadMedia() {
+    let url = document.getElementById("urlInput").value;
+
+    fetch("https://download-all.onrender.com", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ url: url })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            window.location.href = data.download_link;
+        } else {
+            document.getElementById("message").innerText = "Download failed.";
+        }
+    });
+}
